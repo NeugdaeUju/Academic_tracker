b61f1628bad71cb3a956b52826d8463447618d39
Leïla Pelletier <leila.plltr@gmail.com>
2026-06-12 14:37:39 +0200

    Création du premier composant : Header avec menu

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,16 @@
+
+
 <script setup>
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <h1 class="titre">AcademicPlanner</h1>
-    <p class="subtitles">This planning is for all students who wants to win!</p>
-  </header>
-  <main>
-    <div class="explications">
-      <p>We're sorry, this web app is in building. Please, wait a little bit longer before using it.</p>
-      <br />
-      <p>To wait, you can see how the app will look!</p>
-      <br />
-      <br />
-      <p>Enjoy !</p>
-    </div>
 
-    <div class="images">
-      <Transition name="fade" appear>
-        <img class='image-vue-1' src="./assets/Desktop-2.png" alt="images de la maquettes du site">
-      </Transition>
-      <Transition name="fade2" appear>
-        <img class="image-vue-2" src="./assets/Desktop-1.jpg" alt="image numéro 2 de la maquette du site">
-      </Transition>
-      <Transition name="fade3" appear>
-        <img class="image-vue-3" src="./assets/Desktop-3.png" alt="image numéro 3 de la maquette du site">
-      </Transition>
-    </div>
+  <Header />
+  <main>
   </main>
 </template>
 
+
 <style scoped>
-
-header{
-  /*height : 80px;*/
-  border-bottom : 1px solid var(--text-color);
-  padding : 20px 80px;
-}
-.titre {
-  color : #977f3f;
-  margin-bottom : 12px;
-}
-
-main {
-  margin: 20px;
-  display : flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.explications {
-  text-align: center;
-  margin : 150px 0;
-}
-
-.images {
-  width : 90%;
-  display : flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap : 25px;
-  margin : 20px;
-}
-
-.image-vue-1, .image-vue-2, .image-vue-3 {
-  border: 0.5px solid var(--text-color);
-  border-radius : 10px;
-  width : 100%;
-}
-
-.fade-appear-active, .fade-enter-active {
-  transition : opacity 2000ms ease;
-}
-
-.fade2-appear-active, .fade2-enter-active {
-  transition: opacity 2000ms ease;
-  transition-delay: 200ms; /* démarre après la première */
-}
-
-.fade3-appear-active, .fade3-enter-active {
-  transition: opacity 2000ms ease;
-  transition-delay: 400ms; /* démarre après la seconde */
-}
-
-.fade-appear-from, .fade-enter-from, .fade2-appear-from, .fade2-enter-from, .fade3-appear-from, .fade3-enter-from {
-  opacity : 0;
-}
-
-.fade-appear-to, .fade-enter-to, .fade2-appear-to, .fade2-enter-to, .fade3-appear-to, .fade3-enter-to {
-  opacity : 1;
-}
-
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,180 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="app-block">
+      <img class='logo' src="../assets/logo.svg"/>
+      <div class="texts">
+        <img class='nom-app' src="../assets/nom-logo.svg">
+        <p class="parcours-list">L3 → M1 → M2</p>
+      </div>
+    </div>
+
+    <nav>
+      <h2 class="titre-section">FORMATIONS</h2>
+      <ul class="liste-nav">
+        <li class="item active"><div class="mark"></div>L3 Gestion FAD</li>
+        <li class="item"><div class="mark"></div>M1 PME & Entrepr.</li>
+        <li class="item"><div class="mark"></div>M2 MAE</li>
+      </ul>
+    </nav>
+
+    <div class="progession-block">
+      <div class="titre-prct">
+        <p class="titre-section">Progression globale</p>
+        <span class="pourcentage">18%</span>
+      </div>
+      <div class="progress-bar">
+        <div class="progress-bar-empty"></div>
+        <div class="progress-bar-color"></div>
+      </div>
+    </div>
+    
+  </header>
+</template>
+
+<style scoped>
+
+header {
+  Width : 300px;
+  height : 100vh;
+  position : relative;
+  top : 0;
+  left: 0;
+  background: var(--bg-color);
+  /*padding : 0 24px;*/
+}
+
+/* Block Nom général*/
+.app-block {
+  display : flex;
+  align-items: center;
+  gap : 8px;
+  padding : 37px 24px;
+  border-bottom : 1px solid var(--text-color)
+}
+
+.logo {
+  width : 55px;
+  height : 100%;
+}
+
+.nom-app {
+  width : 184px;
+  height : 1001%;
+}
+
+.parcours-list {
+  margin-left : 8px;
+  color : var(--grey1);
+  font-weight : medium;
+  font-size : 12px;
+}
+
+/* Navigation */
+
+nav {
+  padding : 15px 0;
+}
+
+li::marker {
+  content : none;
+}
+
+.titre-section {
+  font-size : 8px;
+  font-family : 'work sans', sans-serif;
+  color : var(--grey1);
+  margin-bottom : 15px;
+  letter-spacing: 1.75%;
+  font-weight : 600;
+  margin-left : 24px;
+}
+
+.item {
+  font-size : 11px;
+  color : var(--grey2);
+  padding : 15px;
+  display : flex;
+  flex-direction: row;
+  cursor: pointer;
+}
+
+.mark {
+  width : 14px;
+  height : 14px;
+  border-radius: 999px;
+  margin-left : 28px;
+  margin-right : 20px;
+}
+
+.item:nth-child(1) .mark {
+  background-color: var(--violet);
+}
+
+.item:nth-child(2) .mark {
+  background-color: var(--vert);
+}
+
+.item:nth-child(3) .mark {
+  background-color: var(--orange);
+}
+
+.active {
+  background-color: #EEEDFE;
+  border-left : 3px solid var(--violet);
+}
+
+
+/* Block progression globale */
+
+.progession-block {
+  position : absolute ;
+  bottom :0;
+  border-top : 1px solid var(--text-color);
+  padding-top : 15px;
+  font-size : 10px;
+}
+
+.pourcentage {
+  color : var(--grey1);
+  font-size : 10px;
+  margin-bottom : 15px;
+  margin-right : 24px;
+}
+
+.titre-prct {
+  display : flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.titre-prct .titre-section {
+  font-size : 10px;
+}
+
+.progress-bar {
+  width: 280px;
+  margin : 7px 10px 53px 10px;
+  position : relative;
+}
+
+.progress-bar-empty {
+  position : absolute ;
+  height : 4px;
+  width: 100%;
+  background-color : #fff;
+  border-radius: 999px;
+}
+
+.progress-bar-color {
+  position : absolute;
+  background: var(--violet);
+  height : 4px;
+  width: 18%;
+  border-radius: 999px;
+
+}
+
+</style>
